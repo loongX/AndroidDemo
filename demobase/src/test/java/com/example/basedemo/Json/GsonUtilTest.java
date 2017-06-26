d0b691bf072d0a61c7f565892724d7b1f04c942b
longX <long8086@foxmail.com>
2017-06-26 17:45:28 +0800

    Gson解析更新

---
+++ demobase/src/test/java/com/example/basedemo/Json/GsonUtilTest.java
@@ -0,0 +1,53 @@
+package com.example.basedemo.Json;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2017/6/26.
+ */
+public class GsonUtilTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void gsonString() throws Exception {
+
+    }
+
+    @Test
+    public void gsonToBean() throws Exception {
+
+    }
+
+    @Test
+    public void gsonToList() throws Exception {
+
+    }
+
+    @Test
+    public void jsonToList() throws Exception {
+
+    }
+
+    @Test
+    public void gsonToListMaps() throws Exception {
+
+    }
+
+    @Test
+    public void gsonToMaps() throws Exception {
+
+    }
+
+}
