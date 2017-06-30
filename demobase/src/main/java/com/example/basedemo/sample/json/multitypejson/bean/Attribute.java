7b299be5df0728a81c07b866d598bf7fd22f2937
longX <long8086@foxmail.com>
2017-06-30 16:42:52 +0800

    特殊json解析

---
+++ demobase/src/main/java/com/example/basedemo/sample/json/multitypejson/bean/Attribute.java
@@ -0,0 +1,17 @@
+package com.example.basedemo.sample.json.multitypejson.bean;
+
+/**
+ * Created by yunlong.su on 2017/6/13.
+ */
+
+public class Attribute {
+    protected String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ demobase/src/main/java/com/example/basedemo/sample/json/multitypejson/bean/generic/BaseUpperBean.java
@@ -0,0 +1,26 @@
+package com.example.basedemo.sample.json.multitypejson.bean.generic;
+
+/**
+ * Created by yunlong.su on 2017/6/22.
+ */
+
+public class BaseUpperBean<T> {
+    private String type;
+    private T attributes;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public T getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(T attributes) {
+        this.attributes = attributes;
+    }
+}
