0db4d68200a4ec5946d91609dbf57d3311f357b0
loongago <long8086@foxmail.com>
2017-07-05 23:22:15 +0800

    demobase添加listview优化

---
+++ demobase/src/main/java/com/example/basedemo/CategoryAdapter.java
@@ -0,0 +1,77 @@
+package com.example.basedemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class CategoryAdapter extends BaseAdapter {
+	CategoryBean bean;
+	LayoutInflater inflater;
+	Context mContext;
+
+	public CategoryAdapter(Context context, CategoryBean bean) {
+		this.bean = bean;
+		this.inflater = LayoutInflater.from(context);
+		this.mContext = context;
+	}
+	public void onDateChange(CategoryBean bean) {
+		this.bean = bean;
+		this.notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+
+		return bean.getDESCRIBE().length;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		String[] describe = bean.getDESCRIBE();
+		return describe[position];
+	}
+
+	@Override
+	public long getItemId(int position) {
+
+		return position;
+	}
+
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		String[] describe = bean.getDESCRIBE();
+		final Class[] CLAZZ = bean.getCLAZZES();
+
+		ViewHolder holder;
+		if (convertView == null) {
+			holder = new ViewHolder();
+			convertView = inflater.inflate(R.layout.item_category, null);
+			holder.tv_describe = (TextView) convertView
+					.findViewById(R.id.item_class_describe);
+			convertView.setTag(holder);
+		}else{
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.tv_describe.setText(describe[position]);
+		convertView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				launchOther(CLAZZ[position]);
+			}
+		});
+		return convertView;
+	}
+
+	class ViewHolder {
+		TextView tv_describe;
+	}
+
+	private void launchOther(Class other){
+		Intent intent  = new Intent(mContext,other);
+		mContext.startActivity(intent);
+	}
+}
---
+++ demobase/src/main/java/com/example/basedemo/CategoryConstant1.java
@@ -0,0 +1,26 @@
+package com.example.basedemo;
+
+import com.example.basedemo.listview.ListViewActivity;
+import com.example.basedemo.sample.json.JsonParseActivity;
+import com.example.basedemo.sample.json.multitypejson.MultiTypeJsonActivity;
+
+
+/**
+ * Created by loongago on 2017-06-21.
+ */
+
+public class CategoryConstant1 {
+
+    public   Class[] CLAZZES = new Class[]{
+            ListViewActivity.class,//1
+            JsonParseActivity.class,//2
+            MultiTypeJsonActivity.class,//3
+
+    };
+
+    public String[] DESCRIBE = new String[] {
+            "下拉listview优化",//1
+            "简单json解析",//2
+            "复杂json解析",//2
+    };
+}
