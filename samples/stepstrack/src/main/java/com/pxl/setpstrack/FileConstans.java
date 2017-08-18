e5835767565b438d7ffe42ac424cd56140e418b5
longX <long8086@foxmail.com>
2017-08-18 15:52:18 +0800

    update util

--- samples/stepstrack/src/main/java/com/pxl/setpstrack/FileConstans.java
+++ samples/stepstrack/src/main/java/com/pxl/setpstrack/FileConstans.java
@@ -1,8 +1,133 @@
 package com.pxl.setpstrack;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/7/28.
  */
 
 public class FileConstans {
+    /**
+     * 读取文件
+     * @param strFilePath
+     * @return
+     */
+    public static String readTxtFile(String strFilePath) {
+        String path = strFilePath;
+
+        StringBuilder builder = new StringBuilder();
+        //打开文件
+        File file = new File(path);
+        //如果path是传递过来的参数，可以做一个非目录的判断
+        if (file.isDirectory()) {
+            Log.d("", "The " + strFilePath + " File doesn't not exist.");
+        } else {
+            try {
+                InputStream instream = new FileInputStream(file);
+                if (instream != null) {
+                    InputStreamReader inputreader = new InputStreamReader(instream);
+                    BufferedReader buffreader = new BufferedReader(inputreader);
+                    String line;
+                    //分行读取
+                    while ((line = buffreader.readLine()) != null) {
+                        builder.append(line + "\n");
+                    }
+                    instream.close();
+                }
+            } catch (java.io.FileNotFoundException e) {
+                Log.d("", "The " + strFilePath + " File doesn't not exist.");
+            } catch (IOException e) {
+                Log.d("", e.getMessage());
+            }
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 读取sd卡上指定后缀的所有文件
+     * @param files 返回的所有文件
+     * @param filePath 路径(可传入sd卡路径)
+     * @param suffere 后缀名称 比如 .gif
+     * @return
+     */
+    public static List<File> getSuffixFile(List<File> files, String filePath, String suffere) {
+
+        File f = new File(filePath);
+
+        if (!f.exists()) {
+            return null;
+        }
+
+        File[] subFiles = f.listFiles();
+        for (File subFile : subFiles) {
+            if(subFile.isFile() && subFile.getName().endsWith(suffere)){
+                files.add(subFile);
+            } else if(subFile.isDirectory()){
+                getSuffixFile(files, subFile.getAbsolutePath(), suffere);
+            } else{
+                //非指定目录文件 不做处理
+            }
+
+        }
+
+        return files;
+    }
+
+    /**
+     * 删除目录（文件夹）以及目录下的文件
+     * @param   sPath 被删除目录的文件路径
+     * @return  目录删除成功返回true，否则返回false
+     */
+    public static boolean deleteDirectory(String sPath) {
+        //如果sPath不以文件分隔符结尾，自动添加文件分隔符
+        if (!sPath.endsWith(File.separator)) {
+            sPath = sPath + File.separator;
+        }
+        File dirFile = new File(sPath);
+        //如果dir对应的文件不存在，或者不是一个目录，则退出
+        if (!dirFile.exists() || !dirFile.isDirectory()) {
+            return false;
+        }
+        boolean flag = true;
+        //删除文件夹下的所有文件(包括子目录)
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            //删除子文件
+            if (files[i].isFile()) {
+                flag = deleteFile(files[i].getAbsolutePath());
+                if (!flag) break;
+            } //删除子目录
+            else {
+                flag = deleteDirectory(files[i].getAbsolutePath());
+                if (!flag) break;
+            }
+        }
+        if (!flag) return false;
+        //删除当前目录
+        if (dirFile.delete()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean deleteFile(String absolutePath) {
+        File file = new File(absolutePath);
+        if (!file.exists()){
+            return true;
+        }
+        if (file.delete()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- samples/stepstrack/src/main/java/com/pxl/setpstrack/GlobalSession.java
+++ samples/stepstrack/src/main/java/com/pxl/setpstrack/GlobalSession.java
@@ -5,4 +5,5 @@ package com.pxl.setpstrack;
  */
 
 public class GlobalSession {
+
 }
