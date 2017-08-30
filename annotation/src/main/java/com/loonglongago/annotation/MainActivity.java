bae9b64de98d82bd2045f779986335c3d177b74f
longX <long8086@foxmail.com>
2017-08-30 15:50:21 +0800

    update annotation

---
+++ annotation/src/main/java/com/loonglongago/annotation/MainActivity.java
@@ -0,0 +1,41 @@
+package com.loonglongago.annotation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.loonglongago.annotation.view.ViewUtils;
+import com.loonglongago.annotation.view.annotation.ContentView;
+import com.loonglongago.annotation.view.annotation.ViewInject;
+import com.loonglongago.annotation.view.annotation.event.OnClick;
+import com.loonglongago.annotation.R;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @ViewInject(R.id.textView)
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        ViewUtils.injectContentView(this);
+
+        ViewUtils.inject(this);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("成功了！");
+            }
+        });
+
+    }
+
+
+    @OnClick(R.id.tb_2)
+    public void onClick(View v) {
+        textView.setText("你按了button2");
+    }
+}
---
+++ annotation/src/test/java/com/loonglongago/annotation/view/ResTypeTest.java
@@ -0,0 +1,10 @@
+package com.loonglongago.annotation.view;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xlpan on 2017/8/30.
+ */
+public class ResTypeTest {
+
+}
