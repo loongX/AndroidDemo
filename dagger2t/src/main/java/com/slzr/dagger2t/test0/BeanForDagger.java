183eaeb6508a21050f50c3c1ae80eb75dabfcc65
longX <long8086@foxmail.com>
2017-09-14 15:49:22 +0800

    update dagger

---
+++ dagger2t/src/main/java/com/slzr/dagger2t/test0/BeanForDagger.java
@@ -0,0 +1,29 @@
+package com.slzr.dagger2t.test0;
+
+import com.slzr.dagger2t.ParamForDagger;
+
+import javax.inject.Inject;
+
+/**
+ * Created by pxl on 2017/9/14.
+ */
+
+public class BeanForDagger {
+    private String mName = null;
+
+
+
+    public BeanForDagger() {
+        this.mName = "Dagger方式";
+    }
+
+    @Inject
+    public BeanForDagger(ParamForDagger param) {
+        this.mName = param.getName();
+    }
+
+
+    public String getName() {
+        return mName;
+    }
+}
