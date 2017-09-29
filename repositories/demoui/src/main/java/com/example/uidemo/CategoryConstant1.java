8e086cefba8ec9026a0eeecdc7067b989e7a1c88
longX <long8086@foxmail.com>
2017-09-29 11:49:52 +0800

    update combination

--- repositories/demoui/src/main/java/com/example/uidemo/CategoryConstant1.java
+++ repositories/demoui/src/main/java/com/example/uidemo/CategoryConstant1.java
@@ -1,5 +1,7 @@
 package com.example.uidemo;
 
+import com.example.uidemo.Animation.combination.TitleBar;
+import com.example.uidemo.Animation.combination.TitleBarActivity;
 import com.example.uidemo.activitydialog.DialogControl;
 import com.example.uidemo.beziercurve.Beziercurve;
 import com.example.uidemo.listviewfrash.ReFlashListViewMainActivity;
@@ -17,7 +19,8 @@ public class CategoryConstant1 {
             SlideUnlockViewControl.class,//2
             ReFlashListViewMainActivity.class,//3
             Beziercurve.class,//4
-            AnimationMainActivity.class,//4
+            AnimationMainActivity.class,//5
+            TitleBarActivity.class,//6
 
 
     };
@@ -27,7 +30,8 @@ public class CategoryConstant1 {
             "滑动解锁",//2
             "listview下拉刷新",//3
             "贝塞尔曲线",//4
-            "弹性动画"
+            "弹性动画",//5
+            "组合控件",//6
 
     };
 }
